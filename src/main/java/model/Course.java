3206564e30a16b3c4e388563e946403b4235ad4a
AlvaroFernandez <alfergonsgs@gmail.com>
2025-02-12 20:29:26 +0100

    getters and setters added

--- src/main/java/model/Course.java
+++ src/main/java/model/Course.java
@@ -1,6 +1,8 @@
 
 package model;
+
 import java.lang.annotation.Inherited;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +32,6 @@ public class Course {
     @Lob
     private Blob notes;
 
-
     private String instructoriD;
 
     @Min(0)
@@ -38,6 +39,94 @@ public class Course {
     private int rating;
 
     @OneToMany(cascade = CascadeType.ALL)
-	private List<Comment> comments = new ArrayList<>();
+    private List<Comment> comments = new ArrayList<>();
+
+    public Course() {
+    }
+
+    // Constructor con parámetros
+    public Course(String title, String description, String topic, Blob image, Blob notes, String instructorId,
+            int rating) {
+        this.title = title;
+        this.description = description;
+        this.topic = topic;
+        this.image = image;
+        this.notes = notes;
+        this.instructorId = instructorId;
+        this.rating = rating;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Blob getImage() {
+        return image;
+    }
+
+    public void setImage(Blob image) {
+        this.image = image;
+    }
+
+    public Blob getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Blob notes) {
+        this.notes = notes;
+    }
+
+    public String getInstructorId() {
+        return instructorId;
+    }
+
+    public void setInstructorId(String instructorId) {
+        this.instructorId = instructorId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 
 }
