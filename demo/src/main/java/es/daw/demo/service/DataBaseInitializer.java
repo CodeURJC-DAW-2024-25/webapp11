75fec38f1d152357b2977ac8be14e0dcf8bef4db
eloydsdlh <e.desande.2021@alumnos.urjc.es>
2025-03-01 18:58:53 +0100

    Add pictures for all courses

--- demo/src/main/java/es/daw/demo/service/DataBaseInitializer.java
+++ demo/src/main/java/es/daw/demo/service/DataBaseInitializer.java
@@ -30,56 +30,79 @@ public class DataBaseInitializer{
     public void initializeDatabase() throws IOException {
 
         User user1 = new User("Pepe", "García", "user1@gmail.com", passwordEncoder.encode("1234"), "Desarrollo web", "USER");
-        User user2 = new User("Ana", "López", "admin@gmail.com", passwordEncoder.encode("1234"), "Desarrollo web", "ADMIN");
+        User user2 = new User("Juan", "García", "user2@gmail.com", passwordEncoder.encode("1234"), "Desarrollo móvil", "USER");
+        User user3 = new User("Antonio", "García", "user3@gmail.com", passwordEncoder.encode("1234"), "Finanzas", "USER");
+        User user4 = new User("Pedro", "García", "user4@gmail.com", passwordEncoder.encode("1234"), "Liderazgo", "USER");
+        User user5 = new User("Felipe", "García", "user5@gmail.com", passwordEncoder.encode("1234"), "Comunicación", "USER");
+        User user6 = new User("María", "García", "user6@gmail.com", passwordEncoder.encode("1234"), "Emprendimiento", "USER");
+        User user7 = new User("Inés", "García", "user7@gmail.com", passwordEncoder.encode("1234"), "Desarrollo móvil", "USER");
+        User user8 = new User("Irene", "García", "user8@gmail.com", passwordEncoder.encode("1234"), "Desarrollo web", "USER");
+        User user9 = new User("Natalia", "García", "user9@gmail.com", passwordEncoder.encode("1234"), "Desarrollo web", "USER");
+        User admin = new User("Ana", "López", "admin@gmail.com", passwordEncoder.encode("1234"), "Desarrollo web", "ADMIN");
 
-        //userService.save(user1);
-        //userService.save(user2);
+    
         setProfileImage(user1, "/static/images/perfil_1.jpg");
-        setProfileImage(user2, "/static/images/perfil_2.jpg");
+        setProfileImage(user2, "/static/images/perfil_1.jpg");
+        setProfileImage(user3, "/static/images/perfil_1.jpg");
+        setProfileImage(user4, "/static/images/perfil_1.jpg");
+        setProfileImage(user5, "/static/images/perfil_1.jpg");
+        setProfileImage(user6, "/static/images/perfil_2.jpg");
+        setProfileImage(user7, "/static/images/perfil_2.jpg");
+        setProfileImage(user8, "/static/images/perfil_2.jpg");
+        setProfileImage(user9, "/static/images/perfil_2.jpg");
+        setProfileImage(admin, "/static/images/perfil_2.jpg");
         userService.save(user1);
         userService.save(user2);
+        userService.save(user3);
+        userService.save(user4);
+        userService.save(user5);
+        userService.save(user6);
+        userService.save(user7);
+        userService.save(user8);
+        userService.save(user9);
+        userService.save(admin);
 
         
-        Course course1 = new Course("Desarrollo Android desde cero", "Aprende a desarrollar aplicaciones móviles para Android.", "Desarrollo móvil", user2);
+        Course course1 = new Course("Desarrollo Android desde cero", "Aprende a desarrollar aplicaciones móviles para Android.", "Desarrollo móvil", user1);
         Course course2 = new Course("iOS para principiantes", "Introducción al desarrollo de apps para iPhone y iPad.", "Desarrollo móvil", user2);
-        Course course3 = new Course("Flutter y Dart", "Crea aplicaciones multiplataforma con Flutter y Dart.", "Desarrollo móvil", user2);
-        Course course4 = new Course("React Native avanzado", "Domina React Native para crear aplicaciones móviles híbridas.", "Desarrollo móvil", user2);
+        Course course3 = new Course("Flutter y Dart", "Crea aplicaciones multiplataforma con Flutter y Dart.", "Desarrollo móvil", user3);
+        Course course4 = new Course("React Native avanzado", "Domina React Native para crear aplicaciones móviles híbridas.", "Desarrollo móvil", user4);
 
-        Course course5 = new Course("Introducción al desarrollo de videojuegos", "Aprende los fundamentos del desarrollo de videojuegos.", "Desarrollo de videojuegos", user1);
-        Course course6 = new Course("Unity desde cero", "Crea videojuegos en 2D y 3D con Unity.", "Desarrollo de videojuegos", user1);
-        Course course7 = new Course("Desarrollo de videojuegos con Unreal Engine", "Aprende a crear juegos con Unreal Engine.", "Desarrollo de videojuegos", user1);
-        Course course8 = new Course("Programación de videojuegos con Godot", "Explora el desarrollo de videojuegos con el motor Godot.", "Desarrollo de videojuegos", user1);
+        Course course5 = new Course("Introducción al desarrollo de videojuegos", "Aprende los fundamentos del desarrollo de videojuegos.", "Desarrollo de videojuegos", user5);
+        Course course6 = new Course("Unity desde cero", "Crea videojuegos en 2D y 3D con Unity.", "Desarrollo de videojuegos", user6);
+        Course course7 = new Course("Desarrollo de videojuegos con Unreal Engine", "Aprende a crear juegos con Unreal Engine.", "Desarrollo de videojuegos", user7);
+        Course course8 = new Course("Programación de videojuegos con Godot", "Explora el desarrollo de videojuegos con el motor Godot.", "Desarrollo de videojuegos", user8);
 
-        Course course9 = new Course("Inicia tu emprendimiento", "Conoce los primeros pasos para lanzar tu negocio.", "Emprendimiento", user1);
+        Course course9 = new Course("Inicia tu emprendimiento", "Conoce los primeros pasos para lanzar tu negocio.", "Emprendimiento", user9);
         Course course10 = new Course("Estrategias de crecimiento para startups", "Cómo escalar tu negocio emergente.", "Emprendimiento", user1);
-        Course course11 = new Course("Modelos de negocio innovadores", "Descubre diferentes modelos de negocio para emprender.", "Emprendimiento", user1);
-        Course course12 = new Course("Marketing para emprendedores", "Aprende estrategias de marketing para hacer crecer tu negocio.", "Emprendimiento", user1);
+        Course course11 = new Course("Modelos de negocio innovadores", "Descubre diferentes modelos de negocio para emprender.", "Emprendimiento", user2);
+        Course course12 = new Course("Marketing para emprendedores", "Aprende estrategias de marketing para hacer crecer tu negocio.", "Emprendimiento", user3);
 
-        Course course13 = new Course("Finanzas personales", "Aprende a gestionar tus finanzas y ahorrar inteligentemente.", "Finanzas", user1);
-        Course course14 = new Course("Inversiones para principiantes", "Conoce los fundamentos de la inversión.", "Finanzas", user1);
-        Course course15 = new Course("Criptomonedas y blockchain", "Explora el mundo de las criptomonedas y la tecnología blockchain.", "Finanzas", user1);
-        Course course16 = new Course("Gestión financiera empresarial", "Cómo manejar las finanzas de una empresa.", "Finanzas", user1);
+        Course course13 = new Course("Finanzas personales", "Aprende a gestionar tus finanzas y ahorrar inteligentemente.", "Finanzas", user4);
+        Course course14 = new Course("Inversiones para principiantes", "Conoce los fundamentos de la inversión.", "Finanzas", user5);
+        Course course15 = new Course("Criptomonedas y blockchain", "Explora el mundo de las criptomonedas y la tecnología blockchain.", "Finanzas", user6);
+        Course course16 = new Course("Gestión financiera empresarial", "Cómo manejar las finanzas de una empresa.", "Finanzas", user7);
 
-        Course course17 = new Course("Introducción al marketing digital", "Aprende las bases del marketing en línea.", "Marketing Digital", user1);
-        Course course18 = new Course("SEO y posicionamiento web", "Optimiza tu sitio web para los motores de búsqueda.", "Marketing Digital", user1);
+        Course course17 = new Course("Introducción al marketing digital", "Aprende las bases del marketing en línea.", "Marketing Digital", user8);
+        Course course18 = new Course("SEO y posicionamiento web", "Optimiza tu sitio web para los motores de búsqueda.", "Marketing Digital", user9);
         Course course19 = new Course("Publicidad en redes sociales", "Crea campañas publicitarias en Facebook, Instagram y más.", "Marketing Digital", user1);
-        Course course20 = new Course("Email marketing efectivo", "Diseña estrategias de email marketing para captar clientes.", "Marketing Digital", user1);
+        Course course20 = new Course("Email marketing efectivo", "Diseña estrategias de email marketing para captar clientes.", "Marketing Digital", user2);
 
-        Course course21 = new Course("Liderazgo efectivo", "Desarrolla habilidades de liderazgo para guiar equipos.", "Liderazgo", user1);
-        Course course22 = new Course("Gestión de equipos", "Aprende a gestionar equipos de trabajo de manera eficiente.", "Liderazgo", user1);
-        Course course23 = new Course("Liderazgo en tiempos de crisis", "Cómo ser un líder en momentos difíciles.", "Liderazgo", user1);
-        Course course24 = new Course("Toma de decisiones estratégicas", "Mejora tu capacidad de tomar decisiones acertadas.", "Liderazgo", user1);
+        Course course21 = new Course("Liderazgo efectivo", "Desarrolla habilidades de liderazgo para guiar equipos.", "Liderazgo", user3);
+        Course course22 = new Course("Gestión de equipos", "Aprende a gestionar equipos de trabajo de manera eficiente.", "Liderazgo", user4);
+        Course course23 = new Course("Liderazgo en tiempos de crisis", "Cómo ser un líder en momentos difíciles.", "Liderazgo", user5);
+        Course course24 = new Course("Toma de decisiones estratégicas", "Mejora tu capacidad de tomar decisiones acertadas.", "Liderazgo", user6);
 
-        Course course25 = new Course("Comunicación efectiva", "Desarrolla habilidades para comunicarte con claridad.", "Comunicación", user1);
-        Course course26 = new Course("Hablar en público", "Mejora tu confianza y técnicas para hablar en público.", "Comunicación", user1);
-        Course course27 = new Course("Negociación y persuasión", "Aprende técnicas de negociación y persuasión efectivas.", "Comunicación", user1);
+        Course course25 = new Course("Comunicación efectiva", "Desarrolla habilidades para comunicarte con claridad.", "Comunicación", user7);
+        Course course26 = new Course("Hablar en público", "Mejora tu confianza y técnicas para hablar en público.", "Comunicación", user8);
+        Course course27 = new Course("Negociación y persuasión", "Aprende técnicas de negociación y persuasión efectivas.", "Comunicación", user9);
         Course course28 = new Course("Comunicación en el ámbito laboral", "Mejora la comunicación dentro de tu entorno profesional.", "Comunicación", user1);
 
-        Course course29 = new Course("Desarrollo Web Completo", "Aprende desarrollo web desde cero con HTML, CSS, JavaScript, Node.js, y más.", "Desarrollo web",user1);
-        Course course30 = new Course("React JS desde Cero", "Domina React JS y crea aplicaciones web modernas y reactivas. Incluye proyectos prácticos.", "Desarrollo web", user2);
-        Course course31 = new Course("PHP y MySQL Profesional", "Aprende a crear aplicaciones web dinámicas con PHP y MySQL. Incluye integración con APIs.", "Desarrollo web",user1);
-        Course course32 = new Course("Python desde cero", "Domina Python desde cero. Incluye proyectos prácticos.", "Desarrollo web", user2);
-        Course course33 = new Course("Java desde", "Aprende a crear aplicaciones con Java.", "Desarrollo web",user1);
+        Course course29 = new Course("Desarrollo Web Completo", "Aprende desarrollo web desde cero con HTML, CSS, JavaScript, Node.js, y más.", "Desarrollo web",user2);
+        Course course30 = new Course("React JS desde Cero", "Domina React JS y crea aplicaciones web modernas y reactivas. Incluye proyectos prácticos.", "Desarrollo web", user3);
+        Course course31 = new Course("PHP y MySQL Profesional", "Aprende a crear aplicaciones web dinámicas con PHP y MySQL. Incluye integración con APIs.", "Desarrollo web",user4);
+        Course course32 = new Course("Python desde cero", "Domina Python desde cero. Incluye proyectos prácticos.", "Desarrollo web", user5);
+        Course course33 = new Course("Java desde", "Aprende a crear aplicaciones con Java.", "Desarrollo web",user6);
         
         setCourseImage(course1, "/static/images/movil1.jpg");
         setCourseImage(course2, "/static/images/movil2.jpg");
@@ -95,6 +118,32 @@ public class DataBaseInitializer{
         setCourseImage(course10, "/static/images/emp2.jpg");
         setCourseImage(course11, "/static/images/emp3.jpg");
         setCourseImage(course12, "/static/images/emp4.jpg");
+
+        setCourseImage(course13, "/static/images/fin1.jpg");
+        setCourseImage(course14, "/static/images/fin2.jpg");
+        setCourseImage(course15, "/static/images/fin3.jpg");
+        setCourseImage(course16, "/static/images/fin4.jpg");
+
+        setCourseImage(course17, "/static/images/mark1.jpg");
+        setCourseImage(course18, "/static/images/mark2.jpg");
+        setCourseImage(course19, "/static/images/mark3.jpg");
+        setCourseImage(course20, "/static/images/mark4.jpg");
+
+        setCourseImage(course21, "/static/images/lid1.jpg");
+        setCourseImage(course22, "/static/images/lid2.jpg");
+        setCourseImage(course23, "/static/images/emp3.jpg");
+        setCourseImage(course24, "/static/images/emp4.jpg");
+
+        setCourseImage(course25, "/static/images/movil1.jpg");
+        setCourseImage(course26, "/static/images/movil2.jpg");
+        setCourseImage(course27, "/static/images/movil3.jpg");
+        setCourseImage(course28, "/static/images/movil4.jpg");
+
+        setCourseImage(course29, "/static/images/web1.jpg");
+        setCourseImage(course30, "/static/images/web2.jpg");
+        setCourseImage(course31, "/static/images/web3.jpg");
+        setCourseImage(course32, "/static/images/web4.jpg");
+        setCourseImage(course33, "/static/images/web5.jpg");
 
         setCourseNotes(course1, "/static/images/ejemplo.pdf");
         setCourseNotes(course2, "/static/images/ejemplo.pdf");
