6042ca961867a02c75432ff3e8d61dd1a4ce570b
alvaro-f-g <alfergonsgs@gmail.com>
2025-03-03 12:02:42 +0100

    Improve how profile path is tretaed depending on the user

--- demo/src/main/java/es/daw/demo/security/WebSecurityConfig.java
+++ demo/src/main/java/es/daw/demo/security/WebSecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
 @Configuration
 @EnableWebSecurity
@@ -17,8 +19,9 @@ public class WebSecurityConfig {
 	@Autowired
 	RepositoryUserDetailsService userDetailsService;
 
-	@Autowired
-	CourseStatisticsAuthorizationManager courseStatisticsAuth; // Inyectamos el Custom Authorization Manager
+	// @Autowired
+	// AuthorizationManager<RequestAuthorizationContext> courseStatisticsAuth; //
+	// Custom Authorization Manager inyected
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -51,15 +54,13 @@ public class WebSecurityConfig {
 						.requestMatchers("/index/**").permitAll()
 						.requestMatchers("/error").permitAll()
 						.requestMatchers("/css/**").permitAll()
+						// .requestMatchers("/**").permitAll() // Esto hay que quitarlo, pero sin esto
+						// no
+						// funciona
 						// PRIVATE PAGES
 						.requestMatchers("/new_course").hasAnyRole("USER")
 						.requestMatchers("/edit_course/*").hasAnyRole("USER")
-						.requestMatchers("/profile").hasAnyRole("USER")
-						.requestMatchers("/profile/taughtCourses/**").hasAnyRole("USER")
-						// .requestMatchers("/course/*/statistics").access(courseStatisticsAuth) // usa
-						// la lógica personalizada
-						.requestMatchers("/course/*/statistics").hasAnyRole("USER") // Ns como hacer
-						// que solo el profesor/admin pueda ver las estadisticas
+						.requestMatchers("/profile/**").hasAnyRole("USER")
 						.requestMatchers("/admin/*").hasAnyRole("ADMIN"))
 				.formLogin(formLogin -> formLogin
 						.loginPage("/login")
