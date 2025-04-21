8d89cf9497ad98ffde8247edb7d0296ab245d685
gonfergon <g.fernandezg.2020@alumnos.urjc.es>
2025-04-21 12:48:47 +0200

    chartCourse.service Added

---
+++ Cognite/src/app/services/chartCourse.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface CourseRatingChartData {
+  labels: string[];
+  values: number[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChartCourseService {
+  constructor(private http: HttpClient) {}
+
+  getCourseRatings(courseId: number): Observable<CourseRatingChartData> {
+    return this.http.get<CourseRatingChartData>(`/api/v1/charts/course/${courseId}`);
+  }
+}
