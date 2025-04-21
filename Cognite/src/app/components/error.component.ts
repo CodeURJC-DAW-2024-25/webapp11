cc8bd1b1fcec95f61ee34c74546cf46a57b2708f
gonfergon <g.fernandezg.2020@alumnos.urjc.es>
2025-04-21 12:34:32 +0200

    error.component.ts Added

---
+++ Cognite/src/app/components/error.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { ErrorService } from '../services/error.service';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css']
+})
+export class ErrorComponent {
+  errorTitle: string | null = '';
+  errorMessage: string | null = '';
+
+  constructor(private location: Location, private errorService: ErrorService) {
+    this.errorTitle = this.errorService.getErrorTitle();
+    this.errorMessage = this.errorService.getErrorMessage();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
