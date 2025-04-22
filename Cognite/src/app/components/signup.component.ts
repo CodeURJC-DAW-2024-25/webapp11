710803fde3cb9d26113ec7e4aade8d4f5d62c939
alvaro-f-g <alfergonsgs@gmail.com>
2025-04-22 11:38:07 +0200

    fixed error in signup component ts

--- Cognite/src/app/components/signup.component.ts
+++ Cognite/src/app/components/signup.component.ts
@@ -5,7 +5,7 @@ import { SignupService } from '../services/signup.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['../app.component.css']
 })
 export class SignupComponent {
   signupForm: FormGroup;
