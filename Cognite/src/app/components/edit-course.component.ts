6840ac13f4877426c8906d9b708eb8052ef0f665
olga-ssf <o.somalo.2021@alumnos.urjc.es>
2025-04-26 20:01:17 +0200

    Add modal for course update notifications and refactor submission logic

--- Cognite/src/app/components/edit-course.component.ts
+++ Cognite/src/app/components/edit-course.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CourseService } from '../services/courses.service';
 import { CourseDto } from '../dtos/course.dto';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-edit-course',
@@ -25,7 +26,12 @@ export class EditCourseComponent{
   image: File | null = null;
   pdf: File | null = null;
 
-  constructor(private courseService: CourseService, private router: Router, private route: ActivatedRoute) {
+    @ViewChild('modalContent') modalContent!: TemplateRef<any>;  // Usar ng-template
+    modalTitle = '';
+    modalMessage = '';
+    modalType: 'success' | 'error' = 'success';
+
+  constructor(private courseService: CourseService, private router: Router, private route: ActivatedRoute, private modalService: NgbModal) {
     const courseId = this.route.snapshot.params['id'];
     this.courseService.getCourseById(courseId).subscribe(
       (course) => {
@@ -55,26 +61,61 @@ export class EditCourseComponent{
 
   onSubmit(): void {
     if (this.image) {
-        this.courseService.updateCourseImage(this.course.id, this.image).subscribe(
-          () => {
+        this.courseService.updateCourseImage(this.course.id, this.image).subscribe({
+          next: () => {
             console.log('Imagen actualizada con éxito');
+            if (this.pdf) {
+              this.courseService.updateCourseNotes(this.course.id, this.pdf).subscribe({
+                next: () => {
+                  console.log('Notas actualizadas con éxito');
+                  this.courseService.updateCourse(this.course).subscribe(() => {
+                    this.showModal('Curso actualizado', 'El curso y sus archivos se han actualizado exitosamente.', 'success');
+                  });
+                }
+              });
+            } else {
+              this.courseService.updateCourse(this.course).subscribe(() => {
+                this.showModal('Curso actualizado', 'El curso se ha actualizado exitosamente.', 'success');
+              });
+            }
           }
-        );
-    }
-    if (this.pdf) {
-        this.courseService.updateCourseNotes(this.course.id, this.pdf).subscribe(
-          () => {
+        });
+    } else if (this.pdf) {
+        this.courseService.updateCourseNotes(this.course.id, this.pdf).subscribe({
+          next: () => {
             console.log('Notas actualizadas con éxito');
+            this.courseService.updateCourse(this.course).subscribe(() => {
+              this.showModal('Curso actualizado', 'El curso y sus archivos se han actualizado exitosamente.', 'success');
+            });
           }
-        );
+        });
     }
-    this.courseService.updateCourse(this.course).subscribe(() => {
-      alert('Curso actualizado con éxito');
-      this.router.navigate(['/courses']);
-    });
+    else {
+        this.courseService.updateCourse(this.course).subscribe(() => {
+          this.showModal('Curso actualizado', 'El curso se ha actualizado exitosamente.', 'success');
+        });
+    }
+    
+    
   }
 
   onCancel(): void {
     this.router.navigate(['/courses']);
   }
+
+  showModal(title: string, message: string, type: 'success' | 'error' = 'success') {
+    this.modalTitle = title;
+    this.modalMessage = message;
+    this.modalType = type;
+    console.log(this.modalContent)
+    const modalRef = this.modalService.open(this.modalContent);  // Abrir modal
+
+    // Si es un mensaje de éxito, redirigimos después de 5 segundos
+    if (type === 'success') {
+    setTimeout(() => {
+        modalRef.close();  // Cerrar modal después de 5 segundos
+        this.router.navigate(['/users/me']);  // Redirigir a la ruta /courses
+    }, 5000);  // 5000 ms = 5 segundos
+    }
+}
 }
